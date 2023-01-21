fb571065c9c779bd2faded74a9253b8d2f0b0965
Greenskiii <alexdanevich@icloud.com>
2023-01-21 22:53:22 +0200

    Coordinator Updated

---
+++ Diploma_v2/Сoordinator/AppCoordinator.swift
@@ -0,0 +1,62 @@
+//
+//  AppCoordinator.swift
+//  Diploma_v2
+//
+//  Created by Алексей Даневич on 18.01.2023.
+//
+
+import UIKit
+import SwiftUI
+
+class AppCoordinator: Coordinator {
+    @AppStorage("log_status") var logStatus = false
+
+    var parentCoordinator: Coordinator?
+    var children: [Coordinator] = []
+    var viewController: UIViewController
+    var navigationController: UINavigationController? {
+        viewController as? UINavigationController
+    }
+    
+    required init(viewController: UIViewController, parentCoordinator: Coordinator) {
+        self.parentCoordinator = parentCoordinator
+        self.viewController = viewController
+    }
+    
+    init(navCon: UINavigationController) {
+        self.viewController = navCon
+    }
+    
+    func start() {
+        withAnimation {
+            if logStatus {
+                goToMainPageScreen()
+            } else {
+                goToAuthScreen()
+            }
+        }
+    }
+    
+    func goToAuthScreen() {
+        children.removeAll()
+        
+        let viewController = AuthorizationViewController(viewModel: AuthorizationViewModel(model: AuthorizationModel(authManager: AuthManager(), goToMainMenu: {
+            self.goToMainPageScreen()
+        })))
+        
+        let coordinator = AuthorizationCoordinator(viewController: viewController, parentCoordinator: self)
+        children.append(coordinator)
+        navigationController?.viewControllers = [viewController]
+    }
+    
+    func goToMainPageScreen() {
+        print("goToMainPageScreen")
+        children.removeAll()
+        
+        let viewController = MainMenuViewController()
+        let coordinator = MainMenuCoordinator(viewController: viewController, parentCoordinator: self)
+        children.append(coordinator)
+        navigationController?.viewControllers = [viewController]
+    }
+    
+}
