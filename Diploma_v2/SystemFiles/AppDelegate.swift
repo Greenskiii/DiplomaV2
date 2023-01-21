e8f067412a3fb92acea41db1448ec447d6dc4c8e
Greenskiii <alexdanevich@icloud.com>
2023-01-21 22:53:00 +0200

    SystemFiles Updated

---
+++ Diploma_v2/SystemFiles/AppDelegate.swift
@@ -0,0 +1,44 @@
+//
+//  AppDelegate.swift
+//  Diploma_v2
+//
+//  Created by Алексей Даневич on 16.01.2023.
+//
+
+import UIKit
+import FirebaseDatabase
+import FirebaseCore
+import GoogleSignIn
+
+@main
+final class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        FirebaseApp.configure()
+          Database.database().isPersistenceEnabled = true
+        return true
+    }
+    
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        let sessionRole = connectingSceneSession.role
+        let sceneConfig = UISceneConfiguration(name: nil, sessionRole: sessionRole)
+        sceneConfig.delegateClass = SceneDelegate.self
+        return sceneConfig
+    }
+    
+    func application(_ application: UIApplication,
+                     open url: URL,
+                     options: [UIApplication.OpenURLOptionsKey: Any])
+      -> Bool {
+          
+      return GIDSignIn.sharedInstance.handle(url)
+    }
+}
+
---
+++ Diploma_v2/SystemFiles/SceneDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  SceneDelegate.swift
+//  Diploma_v2
+//
+//  Created by Алексей Даневич on 16.01.2023.
+//
+
+import SwiftUI
+
+final class SceneDelegate: NSObject, UIWindowSceneDelegate {
+        
+    var appCoordinator : AppCoordinator?
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let navigationController = UINavigationController()
+
+        appCoordinator = AppCoordinator(navCon: navigationController)
+        appCoordinator!.start()
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+    }
+}
