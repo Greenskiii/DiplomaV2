02d451eccef53d8ae1e8819682d60c177674f73e
Greenskiii <alexdanevich@icloud.com>
2023-02-06 16:18:28 +0200

    SceneDelegate Updated

--- Diploma_v2/SystemFiles/SceneDelegate.swift
+++ Diploma_v2/SystemFiles/SceneDelegate.swift
@@ -16,7 +16,7 @@ final class SceneDelegate: NSObject, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let navigationController = UINavigationController()
-
+        navigationController.navigationBar.isHidden = true
         appCoordinator = AppCoordinator(navCon: navigationController)
         appCoordinator!.start()
         window = UIWindow(windowScene: windowScene)
