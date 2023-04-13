9a6fa69ae8b556613b55d8e308b2c60a3946bfa2
Greenskiii <alexdanevich@icloud.com>
2023-04-13 16:21:15 +0300

    Value Model Updated

--- Diploma_v2/Model/Value.swift
+++ Diploma_v2/Model/Value.swift
@@ -12,20 +12,28 @@ struct Value: Codable, Hashable {
     var name: String
     var value: String
     let imageSystemName: String
+    let minValue: String
+    let maxValue: String
     
     init(name: String,
          value: String,
-         imageSystemName: String
+         imageSystemName: String,
+         minValue: String,
+         maxValue: String
     ) {
         self.name = name
         self.value = value
         self.imageSystemName = imageSystemName
+        self.minValue = minValue
+        self.maxValue = maxValue
     }
     
     init?(dictionary: [String: Any]) {
         guard let name = dictionary["name"] as? String,
               let value = dictionary["value"] as? String,
-              let imageSystemName = dictionary["imageSystemName"] as? String
+              let imageSystemName = dictionary["imageSystemName"] as? String,
+              let minValue = dictionary["minValue"] as? String,
+              let maxValue = dictionary["maxValue"] as? String
         else {
             return nil
         }
@@ -33,5 +41,7 @@ struct Value: Codable, Hashable {
         self.name = name
         self.value = value
         self.imageSystemName = imageSystemName
+        self.minValue = minValue
+        self.maxValue = maxValue
     }
 }
